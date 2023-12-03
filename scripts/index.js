7d4a52c773702ee7bbb94a0d3f1e874f2edb0967
Denis Voinovich <denzelvojn@gmail.com>
2023-12-03 20:48:22 +0300

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,31 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
-let cardElement;
 
 const createCard = (cards) => {
+  const cardElements = [];
+
   cards.forEach(element => {
-    cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = `${element.name} пейзаж`;
     cardElement.querySelector('.card__title').textContent = element.name;
-    placesList.append(cardElement)
-  })
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunc);
+    cardElements.push(cardElement);
+  });
+
+  return cardElements;
 }
-
-createCard(initialCards)
-
-const cardDeleteButton = document.querySelectorAll('.card__delete-button');
 
 const deleteFunc = (event) => {
   const card = event.target.closest('.card');
   card.remove();
 }
 
-cardDeleteButton.forEach((button) => {
-  button.addEventListener('click', deleteFunc);
-});
+const renderCards = (cardElements) => {
+  cardElements.forEach(cardElement => {
+    placesList.append(cardElement);
+  });
+}
+
+const cards = createCard(initialCards);
+renderCards(cards);
