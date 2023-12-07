27a546ec159f77d2bb808dfaebfd4d2f51f8fb3e
Denis Voinovich <denzelvojn@gmail.com>
2023-12-07 15:02:06 +0300

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
-const createCard = (cards) => {
+const createCard = (cards, callBackDeleteCard) => {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = cards.link;
   cardElement.querySelector('.card__image').alt = `${cards.name}: пейзаж`;
   cardElement.querySelector('.card__title').textContent = cards.name;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunc);
+  cardElement.querySelector('.card__delete-button').addEventListener('click', callBackDeleteCard);
   return cardElement
 }
 
