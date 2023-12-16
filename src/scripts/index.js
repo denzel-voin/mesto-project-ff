2935e0ffacec9b4ed7879ecf3af9f3c8869f6c30
Denis Voinovich <denzelvojn@gmail.com>
2023-12-16 21:04:07 +0300

    add popup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,7 +18,7 @@ const placesList = document.querySelector('.places__list');
 const createCard = (cards, callBackDeleteCard) => {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = cards.link;
-  cardElement.querySelector('.card__image').alt = `${cards.name}: пейзаж`;
+  cardElement.querySelector('.card__image').alt = `${cards.name}`;
   cardElement.querySelector('.card__title').textContent = cards.name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', callBackDeleteCard);
   return cardElement
@@ -36,3 +36,38 @@ const renderCards = (cardElement) => {
 initialCards.forEach((card) => {
   renderCards(createCard(card, deleteFunc))
 });
+
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelectorAll('.popup__close');
+const editPopup = document.querySelector('.popup_type_edit');
+const addButton = document.querySelector('.profile__add-button');
+const addNewCard = document.querySelector('.popup_type_new-card');
+const cardImage = document.querySelectorAll('.card__image');
+const popUpTypeImage = document.querySelector('.popup_type_image');
+
+
+cardImage.forEach(element => {
+  element.addEventListener('click', () => {
+    popUpTypeImage.style.display = 'flex';
+    document.querySelector('.popup__image').src = element.src;
+    document.querySelector('.popup__caption').textContent = element.alt
+  })
+})
+
+editButton.addEventListener('click', () => {
+  editPopup.style.display = 'flex'
+});
+
+closeButton.forEach(element => {
+  element.addEventListener('click', () => {
+    editPopup.style.display = 'none';
+    addNewCard.style.display = 'none';
+    popUpTypeImage.style.display = 'none';
+  })
+})
+
+
+addButton.addEventListener('click', () => {
+  addNewCard.style.display = 'flex'
+});
+
