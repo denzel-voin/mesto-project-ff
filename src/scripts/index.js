2f97fb3a86714da9d2faea5b5c8904e46de4c137
Denis Voinovich <denzelvojn@gmail.com>
2023-12-16 16:36:22 +0300

    add webpack config

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,17 @@
+import avatar from '../images/avatar.jpg'
+import logo from '../images/logo.svg'
+import '../pages/index.css';
+import initialCards from './cards'
+
+const loadImages = () => {
+  const headerLogo = document.querySelector('.header__logo');
+  const profileImage = document.querySelector('.profile__image');
+
+  headerLogo.setAttribute('src', logo);
+  profileImage.style.backgroundImage = `url(${avatar})`;
+}
+loadImages()
+
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
